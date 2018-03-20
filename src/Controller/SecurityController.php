f4bc9203a8e9c0bcffb0a3adefd27afecef75948
Roland Golla <rolandgolla@gmail.com>
2018-03-20 10:43:12 +0100

    go forward spam protection

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,5 +1,6 @@
 <?php
 
+
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
@@ -10,18 +11,22 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends Controller
 {
     /**
+     * @param Request             $request   This is the request object from symfony.
+     * @param AuthenticationUtils $authUtils Tdhajhcj.
      * @Route("/community/login", name="login")
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function login(Request $request, AuthenticationUtils $authUtils)
     {
         $error = $authUtils->getLastAuthenticationError();
 
-        $lastUsername = $authUtils->getLastUsername();
-
-        return $this->render('community/login.html.twig', array(
+        return $this->render(
+            'community/login.html.twig',
+            array(
             'last_username' => $lastUsername,
             'error'         => $error,
-        ));
+            )
+        );
     }
 
     /**
@@ -30,5 +35,31 @@ class SecurityController extends Controller
     public function logout(): void
     {
         throw new \Exception('This should never be reached!');
+    }
+
+    public function spamProtection(array $data)
+    {
+        // Validate IP
+        if(!$this->validateIp($data['ip'])) {
+            return false;
+        }
+
+        // Validate post limit
+
+        // Validate message
+
+        // Validate Email
+
+
+    }
+
+    private function validateIp(string $ip)
+    {
+        if(!filter_var($ip, FILTER_VALIDATE_IP)) {
+            return false;
+        }
+
+        // Europe
+
     }
 }
