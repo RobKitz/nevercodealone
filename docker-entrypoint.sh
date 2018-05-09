5bb8118aad682259df968e12308ff03197dac5c7
moenka <moenka@10forge.org>
2018-05-09 11:30:01 +0200

    push

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,9 +7,10 @@ fi
 sudo -u www-data composer install $COMPOSER_FLAGS
 
 if [[ $APP_ENV == 'dev' ]]; then
-    sleep 5
-    bin/console doctrine:schema:create
+#    sleep 5
+#    bin/console doctrine:schema:create
+    exec "$@"
+else
+    /usr/sbin/apachectl -DFOREGROUND
 fi
-
-/usr/sbin/apachectl -DFOREGROUND
 
