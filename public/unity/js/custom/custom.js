60fe24dd2d23288d9dbe1018d43da718fdfdba79
Roland Golla <rolandgolla@gmail.com>
2018-05-10 23:19:52 +0200

    ncaevents - content and style fixes - contact form on every button

--- public/unity/js/custom/custom.js
+++ public/unity/js/custom/custom.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
     /************************************************************************************ CAROUSEL SLIDER STARTS */
 
-    $("#frm_contact").submit(function(e) {
+    $("#nca-form").submit(function(e) {
         e.preventDefault();
         var url = "/api/messages"; // the script where you handle the form input.
 
@@ -20,7 +20,6 @@ $(document).ready(function() {
             }
         );
 
-        console.log(data);
         $.ajax({
             type: "POST",
             url: url,
@@ -28,12 +27,38 @@ $(document).ready(function() {
             dataType:"json",
             data:data,
         }).done(function(data) {
-            $("#frm_contact").html("<div class='successMessage'>Danke wir melden uns</div>");
+            $("#nca-form").html("<div class='successMessage'>Danke wir melden uns</div>");
         }).fail(function(data) {
 
         });
 
         return false;
+    });
+
+    $("#hide, #show, .nca-contact").click(function(e) {
+        if ($("#show").is(":visible")) {
+
+            $("#show").animate({
+                "margin-right": "-400px"
+            }, 500, function() {
+                $(this).hide();
+            });
+
+            $("#switch").animate({
+                "margin-right": "0px"
+            }, 500).show();
+        } else {
+            $("#switch").animate({
+                "margin-right": "-400px"
+            }, 500, function() {
+                $(this).hide();
+            });
+            $("#show").show().animate({
+                "margin-right": "0"
+            }, 500);
+        }
+
+        e.preventDefault();
     });
 
     var owl = $('.single-image-carousel');
