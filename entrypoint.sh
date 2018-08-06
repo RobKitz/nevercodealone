8e8c8694781864d0a0093599512757f5f256aed3
moenka <moenka@10forge.org>
2018-08-06 18:25:02 +0200

    Remove volumes from docker-compose
    Application files must not reside in docker volumes without a proper migration script since file in docker volumes wont get updated on a container upgrade.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,10 @@ set -e
 
 for i in $(env | grep -E "^SYMFONY_"); do
     echo "${i#SYMFONY_}" >> .env
+    echo "${i#SYMFONY_}" >> /etc/environment
 done
+sed -i -E 's/^(.*)=(.*)$/\1="\2"/g' /etc/environment
+source /etc/environment
 chown -R www-data: .
 
 apachectl -DFOREGROUND
