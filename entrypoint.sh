abfbb9de60369a0ff354f7e4d9c144839ab4da9f
moenka <moenka@10forge.org>
2018-08-15 15:25:45 +0200

    Enable on demand https encryption for traefik

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+echo -n "" > .env
 for i in $(env | grep -E "^SYMFONY_"); do
     echo "${i#SYMFONY_}" >> .env
     echo "${i#SYMFONY_}" >> /etc/environment
 done
 sed -i -E 's/^(.*)=(.*)$/\1="\2"/g' /etc/environment
 source /etc/environment
-chown -R www-data: .
+#chown -R www-data: .
 
 apachectl -DFOREGROUND
 
