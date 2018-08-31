9086dd1438bda9e09df1610addb2d78345e9d71e
moenka <moenka@10forge.org>
2018-08-31 12:51:52 +0200

    Add database provisioning to entrypoint if database is not yet initialized.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-[ -n "$APP_DEBUG_FILE" ] && export APP_DEBUG="$(cat $APP_DEBUG_FILE)"
-[ -n "$APP_ENV_FILE" ] && export APP_ENV="$(cat $APP_ENV_FILE)"
-[ -n "$APP_SECRET_FILE" ] && export APP_SECRET="$(cat $APP_SECRET_FILE)"
-[ -n "$CORS_ALLOW_ORIGIN_FILE" ] && export CORS_ALLOW_ORIGIN="$(cat $CORS_ALLOW_ORIGIN_FILE)"
-[ -n "$DATABASE_URL_FILE" ] && export DATABASE_URL="$(cat $DATABASE_URL_FILE)"
+bin/load-env
+sleep 5
+bin/console doctrine:schema:validate || bin/console doctrine:schema:create
 
 exec "$@"
 
