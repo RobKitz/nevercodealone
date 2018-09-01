ac482272d66638798ddd82c33561618b170a8996
Roland Golla <rolandgolla@gmail.com>
2018-09-01 14:34:48 +0200

    metaCest check twitter and fb meta keys if not empty

--- tests/acceptance/page/metaCest.php
+++ tests/acceptance/page/metaCest.php
@@ -10,11 +10,38 @@ class metaCest
         $I->waitForElement('h1');
     }
 
-    // tests
     public function metaContentImageHasStatusOk(AcceptanceTester $I)
     {
         $image = $I->getMeta(['twitter:image'])['twitter:image'];
         $status = $I->getCurlStatusByUrl($image);
         $I->assertSame(200, $status);
+    }
+
+    public function metaContentKeysNotEmpty(AcceptanceTester $I)
+    {
+        $twitterKeys = [
+            'twitter:title',
+            'twitter:description',
+            'twitter:site',
+            'twitter:image',
+            'twitter:creator'
+        ];
+
+        $fbKeys = [
+            'og:locale',
+            'og:type',
+            'og:title',
+            'og:description',
+            'og:url',
+            'og:site_name',
+            'og:image',
+            'fb:app_id'
+        ];
+
+        $metaKeys = array_merge($twitterKeys, $fbKeys);
+
+        foreach ($metaKeys as $metaKey) {
+            $I->assertNotEmpty($I->getMeta([$metaKey]), $metaKey);
+        }
     }
 }
